19431f11f8d37b23421334431564acc223a59469
Seogineer <dgseo8981@gmail.com>
2021-01-07 20:08:53 +0900

    guestbook 실습

---
+++ src/main/java/kr/or/connect/guestbook/config/WebMvcContextConfiguration.java
@@ -0,0 +1,49 @@
+package kr.or.connect.guestbook.config;
+
+import org.springframework.context.annotation.Bean;
+import org.springframework.context.annotation.ComponentScan;
+import org.springframework.context.annotation.Configuration;
+import org.springframework.web.servlet.config.annotation.DefaultServletHandlerConfigurer;
+import org.springframework.web.servlet.config.annotation.EnableWebMvc;
+import org.springframework.web.servlet.config.annotation.ResourceHandlerRegistry;
+import org.springframework.web.servlet.config.annotation.ViewControllerRegistry;
+import org.springframework.web.servlet.config.annotation.WebMvcConfigurerAdapter;
+import org.springframework.web.servlet.view.InternalResourceViewResolver;
+
+@Configuration
+@EnableWebMvc
+@ComponentScan(basePackages = { "kr.or.connect.guestbook.controller" })
+public class WebMvcContextConfiguration extends WebMvcConfigurerAdapter {
+	// URL 요청이 아래 경로와 같은 경우 아래 폴더에서 읽도록 설정
+	@Override
+    public void addResourceHandlers(ResourceHandlerRegistry registry) {
+        registry.addResourceHandler("/assets/**").addResourceLocations("classpath:/META-INF/resources/webjars/").setCachePeriod(31556926);
+        registry.addResourceHandler("/css/**").addResourceLocations("/css/").setCachePeriod(31556926);
+        registry.addResourceHandler("/img/**").addResourceLocations("/img/").setCachePeriod(31556926);
+        registry.addResourceHandler("/js/**").addResourceLocations("/js/").setCachePeriod(31556926);
+    }
+ 
+    // default servlet handler를 사용하게 합니다.
+	// 매핑 정보가 없는 URL 요청이 들어왔을 때 Spring의 DefaultServletHttpRequestHandler가 처리하도록 해준다.
+	// WAS의 default servlet이 static 한 자원을 읽어서 보여줄 수 있게 한다.
+    @Override
+    public void configureDefaultServletHandling(DefaultServletHandlerConfigurer configurer) {
+        configurer.enable();
+    }
+   
+    // 특정 URL에 대한 처리를 컨트롤러 클래스를 작성하지 않고 매핑할 수 있게 한다.
+    @Override
+    public void addViewControllers(final ViewControllerRegistry registry) {
+    	System.out.println("addViewControllers가 호출됩니다. ");
+        registry.addViewController("/").setViewName("index");	// ~/ 요청이 들어오면 index 뷰 호출
+    }
+    
+    @Bean
+    public InternalResourceViewResolver getInternalResourceViewResolver() {
+        InternalResourceViewResolver resolver = new InternalResourceViewResolver();
+        resolver.setPrefix("/WEB-INF/views/");
+        resolver.setSuffix(".jsp");
+        return resolver;
+    }
+}
+
